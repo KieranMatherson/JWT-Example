f61eacf543993affa567b473ce17e229490d9191
Kieran Matherson <KieranM@datacom.co.nz>
2016-06-23 12:42:12 +1200

    Add project to github

---
+++ AuthorizationServer.Api/Models/AudienceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuthorizationServer.Api.Models
+{
+    public class AudienceModel
+    {
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ AuthorizationServer.Api/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using AuthorizationServer.Api.Formats;
+using AuthorizationServer.Api.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+
+namespace AuthorizationServer.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            ConfigureOAuth(app);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            var issuer = ConfigurationManager.AppSettings["issuer"];
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                #if DEBUG
+                AllowInsecureHttp = true,
+                #endif
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat(issuer)
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+
+        }
+    }
+}
